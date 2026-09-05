barrydeen/dark-wisp-android

feat(wallet): withdraw the whole balance on-chain
Adds Withdraw on-chain — wallet settings, danger section, Spark only —
which drains the entire spendable balance to a Bitcoin address.

Built for the user who believes their wallet is broken. It is the escape
hatch that moves their money somewhere they already trust, so it leads
with what will happen, quotes a real fee from the SDK before anything is
signed, and never claims more than it can deliver.

Cooperative, not a unilateral exit. Everything here is ordinary send
machinery aimed at a Bitcoin address — getInfo, prepareSendPayment,
sendPayment, optimizeLeaves — so it needs the Spark operator to
cooperate. It covers the common cases (wallet feels stuck, moving to
another app) but not operator failure. The SDK does expose
prepareUnilateralExit / unilateralExit for that, and it is a much larger
job: fees come from external UTXOs the app does not have, the caller
must implement CpfpSigner.signPsbt, and the caller must broadcast the
transaction set resumably. Hence the naming — this is Withdraw, and
"Exit" stays free for the trustless one.

Uses FeePolicy.FEES_INCLUDED. The SDK default adds fees on top of the
amount, so a send of the full balance can never succeed — there is
nothing left to pay them with. FEES_INCLUDED makes the wallet spend
exactly the amount with the fee taken out of it, which the SDK docs name
as the way to drain a balance. The quote therefore shows three honest
numbers: balance, fee deducted, and what actually lands.

The balance is read with ensureSynced = true — quoting against the
cached figure prices a fee for an amount that may no longer exist.

Quote and execute are split, and that split is the safety property.
prepareWithdrawOnchain returns plain values and keeps the SDK prepared
request private to SparkRepository; executeWithdrawOnchain refuses
unless the quote it is handed still equals the held one. What gets
broadcast is exactly what the user approved, and a drifted screen
re-quotes inst

app/src/main/kotlin/com/darkwisp/app/repo/WithdrawOnchain.kt
@@ -1,0 +1,79 @@
+package com.darkwisp.app.repo
+
+/**
+ * Confirmation speed for an on-chain withdrawal, mapped to the SDK's three
+ * fee tiers. SDK-free so the UI and tests don't depend on the bindings.
+ */
+enum class WithdrawOnchainSpeed(val label: String, val detail: String) {
+    SLOW("Economy", "Cheapest. May take hours to confirm."),
+    MEDIUM("Standard", "Balanced fee and confirmation time."),
+    FAST("Priority", "Highest fee. Confirms soonest.")
+}
+
+/**
+ * What a withdrawal would cost and deliver, quoted before anything is signed.
+ *
+ * [spendSats] is the whole spendable balance and [feeSats] comes out of it -
+ * the SDK's FeePolicy.FEES_INCLUDED - so [netSats] is what actually lands at
+ * the destination. Quoting this way is the only honest way to drain: with
+ * fees added on top, a send of the full balance can never succeed.
+ */
+data class WithdrawOnchainQuote(
+    val address: String,
+    val spendSats: Long,
+    val feeSats: Long,
+    val speed: WithdrawOnchainSpeed
+) {
+    val netSats: Long get() = (spendSats - feeSats).coerceAtLeast(0L)
+
+    /**
+     * True when the fee would consume everything. Spark's dust and fee floors
+     * mean a small balance can be genuinely unspendable on-chain - better to
+     * say so than to broadcast something that delivers zero.
+     */
+    val isUneconomical: Boolean get() = netSats <= 0L
+
+    /**
+     * Share of the balance eaten by fees, for the warning copy. Draining a
+     * small balance can cost a large percentage, and that should be visible
+     * before confirming rather than discovered after.
+     */
+    val feePercent: Double
+        get() = if (spendSats <= 0L) 0.0 else (feeSats.toDouble() / spendSats.toDouble()) * 100.0
+}
+
+/**
+ * Funds a withdrawal cannot move, reported after the fact.
+ *
+ * "Recover everything" is not literally achievable: tokens below the
+ * provider's conversion floor cannot be converted at any price, and Spark
+ * leaves worth less than their own exit cost are not worth spending. Naming
+ * the remainder is more useful than implying it does not exist.
+ */
+data class WithdrawOnchainRemainder(
+    /** Ticker to human-readable amount left behind, e.g. "USDB" to "0.34". */
+    val strandedTokens: Map<String, String> = emptyMap(),
+    val strandedSats: Long = 0L
+) {
+    val isEmpty: Boolean get() = strandedTokens.isEmpty() && strandedSats == 0L
+}
+
+/**
+ * Pull a bare address out of whatever a wallet's QR actually encodes.
+ *
+ * Bitcoin QRs are usually BIP-21 URIs - "bitcoin:bc1q...?amount=0.01&label=x"
+ * - and the SDK's parser wants the address alone. The amount is deliberately
+ * discarded: this screen always sends the whole balance, so honoring a
+ * requested amount would contradict what the button says.
+ */
+fun normalizeBitcoinAddress(raw: String): String {
+    var value = raw.trim()
+    // Anchored and case-insensitive: some wallets emit "BITCOIN:", and an
+    // address containing "bitcoin:" mid-string must not be mangled.
+    if (value.length >= 8 && value.substring(0, 8).equals("bitcoin:", ignoreCase = true)) {
+        value = value.substring(8)
+    }
+    val query = value.indexOf('?')
+    if (query >= 0) value = value.substring(0, query)
+    return value.trim()
+}

app/src/main/kotlin/com/darkwisp/app/ui/screen/WalletScreen.kt
@@ -92,6 +92,7 @@ import androidx.compose.material.icons.filled.Receipt
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.SwapHoriz
+import androidx.compose.material.icons.filled.ArrowOutward
 import androidx.compose.material.icons.filled.CloudUpload
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.AlertDialog
@@ -127,6 +128,7 @@ import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import com.darkwisp.app.ui.component.WithdrawOnchainSheet
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -217,6 +219,21 @@ fun WalletScreen(
 ) {
     val walletState by viewModel.walletState.collectAsState()
     val currentPage by viewModel.currentPage.collectAsState()
+    var showWithdrawOnchain by remember { mutableStateOf(false) }
+
+    if (showWithdrawOnchain) {
+        WithdrawOnchainSheet(
+            onQuote = { address, speed -> viewModel.sparkRepo.prepareWithdrawOnchain(address, speed) },
+            onConfirm = { quote ->
+                val result = viewModel.sparkRepo.executeWithdrawOnchain(quote)
+                // Reflect the emptied balance and the new row without waiting
+                // for the next poll — the user just moved everything.
+                if (result.isSuccess) viewModel.refreshState()
+                result
+            },
+            onDismiss = { showWithdrawOnchain = false }
+        )
+    }
 
     // Always refresh wallet state when this screen appears
     LaunchedEffect(Unit) {
@@ -568,6 +585,12 @@ fun WalletScreen(
                             viewModel.navigateTo(WalletPage.BackupToRelay)
                         },
                         onDeleteWallet = { viewModel.navigateTo(WalletPage.DeleteWalletConfirm) },
+                        // Spark only: passing null hides the row, so an NWC
+                        // wallet can't reach a button with no on-chain send
+                        // command behind it.
+                        onWithdrawOnchain = if (viewModel.walletMode.value == WalletMode.SPARK) {
+                            { showWithdrawOnchain = true }
+                        } else null,
                         relayBackupStatuses = viewModel.relayBackupStatuses.collectAsState().value,
                         relayBackupCheckLoading = viewModel.relayBackupCheckLoading.collectAsState().value,
                         deleteBackupStatus = viewModel.deleteBackupStatus.collectAsState().value,
@@ -4079,6 +4102,8 @@ private fun WalletSettingsContent(
     onBackupMnemonic: () -> Unit,
     onBackupToRelay: () -> Unit = {},
     onDeleteWallet: () -> Unit,
+    /** Spark only — NWC has no on-chain send command. */
+    onWithdrawOnchain: (() -> Unit)? = null,
     relayBackupStatuses: List<RelayBackupInfo> = emptyList(),
     relayBackupCheckLoading: Boolean = false,
     deleteBackupStatus: DeleteBackupStatus = DeleteBackupStatus.Idle,
@@ -4394,6 +4419,44 @@ private fun WalletSettingsContent(
                         style = MaterialTheme.typography.bodyLarge,
                         color = Color(0xFFFF3B30)
                     )
+                }
+            }
+            if (onWithdrawOnchain != null) {
+                Spacer(Modifier.height(8.dp))
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable(onClick = onWithdrawOnchain),
+                    colors = CardDefaults.cardColors(
+                        containerColor = MaterialTheme.colorScheme.surfaceVariant.copy(alpha = 0.5f)
+                    )
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp, vertical = 14.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            Icons.Default.ArrowOutward,
+                            contentDescription = null,
+                            tint = Color(0xFFFF3B30),
+                            modifier = Modifier.size(22.dp)
+                        )
+                        Spacer(Modifier.width(12.dp))
+                        Column {
+                            Text(
+                                "Withdraw on-chain",
+                                style = MaterialTheme.typography.bodyLarge,
+                                color = Color(0xFFFF3B30)
+                            )
+                            Text(
+                                "Send everything to a Bitcoin address",
+                                style = MaterialTheme.typography.bodySmall,
+                                color = MaterialTheme.colorScheme.onSurfaceVariant
+                            )
+                        }
+                    }
                 }
             }
             Spacer(Modifier.height(8.dp))

app/src/test/kotlin/com/darkwisp/app/repo/WithdrawOnchainTest.kt
@@ -1,0 +1,148 @@
+package com.darkwisp.app.repo
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+/**
+ * The arithmetic behind the Withdraw on-chain confirmation screen, and the
+ * BIP-21 address normalization. Pure, and worth pinning: these numbers are
+ * what a user reads before irreversibly emptying their wallet.
+ *
+ * Ported from wisp-ios#452.
+ */
+class WithdrawOnchainTest {
+
+    private fun quote(
+        spend: Long,
+        fee: Long,
+        speed: WithdrawOnchainSpeed = WithdrawOnchainSpeed.MEDIUM
+    ) = WithdrawOnchainQuote("bc1qexample", spend, fee, speed)
+
+    // ---- Quote arithmetic ----
+
+    /** The fee comes OUT of the balance (FEES_INCLUDED). */
+    @Test
+    fun `net is balance minus fee`() {
+        assertEquals(97_500L, quote(100_000, 2_500).netSats)
+    }
+
+    /** Never advertise a negative amount. */
+    @Test
+    fun `net clamps at zero`() {
+        assertEquals(0L, quote(1_000, 5_000).netSats)
+    }
+
+    @Test
+    fun `a fee that eats everything is uneconomical`() {
+        assertTrue(quote(1_000, 5_000).isUneconomical)
+        assertTrue(quote(1_000, 1_000).isUneconomical)
+    }
+
+    @Test
+    fun `an ordinary drain is economical`() {
+        assertFalse(quote(100_000, 2_500).isUneconomical)
+    }
+
+    @Test
+    fun `fee percent is share of balance`() {
+        assertEquals(10.0, quote(100_000, 10_000).feePercent, 0.001)
+        assertEquals(25.0, quote(20_000, 5_000).feePercent, 0.001)
+    }
+
+    /** No division by zero on an empty wallet. */
+    @Test
+    fun `fee percent on an empty balance is zero`() {
+        assertEquals(0.0, quote(0, 500).feePercent, 0.001)
+    }
+
+    /**
+     * Equality gates execution: executeWithdrawOnchain refuses unless the held
+     * quote matches the one the user confirmed, so a drifted screen can't send
+     * different terms than were signed off.
+     */
+    @Test
+    fun `quotes differing in any field are not equal`() {
+        val base = quote(100_000, 2_500)
+        assertTrue(base != quote(100_001, 2_500))
+        assertTrue(base != quote(100_000, 2_600))
+        assertTrue(base != quote(100_000, 2_500, WithdrawOnchainSpeed.FAST))
+        assertTrue(base != WithdrawOnchainQuote("bc1qother", 100_000, 2_500, WithdrawOnchainSpeed.MEDIUM))
+        assertEquals(base, quote(100_000, 2_500))
+    }
+
+    @Test
+    fun `every speed is labelled and explained`() {
+        WithdrawOnchainSpeed.entries.forEach {
+            assertTrue(it.label.isNotEmpty())
+            assertTrue(it.detail.isNotEmpty())
+        }
+    }
+
+    // ---- Remainder ----
+
+    @Test
+    fun `nothing stranded is empty`() {
+        assertTrue(WithdrawOnchainRemainder().isEmpty)
+    }
+
+    /** Tokens under the conversion floor can't move at any price. */
+    @Test
+    fun `stranded tokens are not empty`() {
+        assertFalse(WithdrawOnchainRemainder(strandedTokens = mapOf("USDB" to "0.34")).isEmpty)
+    }
+
+    @Test
+    fun `stranded sats are not empty`() {
+        assertFalse(WithdrawOnchainRemainder(strandedSats = 120).isEmpty)
+    }
+
+    // ---- BIP-21 normalization ----
+
+    @Test
+    fun `a bare address passes through`() {
+        assertEquals("bc1qexample", normalizeBitcoinAddress("bc1qexample"))
+    }
+
+    @Test
+    fun `strips the bip21 scheme`() {
+        assertEquals("bc1qexample", normalizeBitcoinAddress("bitcoin:bc1qexample"))
+    }
+
+    /** Some wallets emit an uppercase scheme. */
+    @Test
+    fun `scheme match is case insensitive`() {
+        assertEquals("bc1qexample", normalizeBitcoinAddress("BITCOIN:bc1qexample"))
+    }
+
+    /**
+     * The amount is discarded on purpose: this screen always sends the whole
+     * balance, so honoring a requested amount would contradict the button.
+     */
+    @Test
+    fun `drops query parameters`() {
+        assertEquals(
+            "bc1qexample",
+            normalizeBitcoinAddress("bitcoin:bc1qexample?amount=0.01&label=x")
+        )
+    }
+
+    @Test
+    fun `trims whitespace and newlines`() {
+        assertEquals("bc1qexample", normalizeBitcoinAddress("  bc1qexample\n"))
+    }
+
+    /** "bitcoin" mid-string is not a scheme - an address must not be mangled. */
+    @Test
+    fun `only an anchored scheme is stripped`() {
+        assertEquals("bc1qbitcoin:x", normalizeBitcoinAddress("bc1qbitcoin:x"))
+    }
+
+    /** Shorter than the scheme itself must not throw. */
+    @Test
+    fun `short input is safe`() {
+        assertEquals("bc1", normalizeBitcoinAddress("bc1"))
+        assertEquals("", normalizeBitcoinAddress(""))
+    }
+}

dmnyc/wisp

app/src/main/kotlin/com/wisp/app/repo/WithdrawOnchain.kt
@@ -1,0 +1,79 @@
+package com.wisp.app.repo
+
+/**
+ * Confirmation speed for an on-chain withdrawal, mapped to the SDK's three
+ * fee tiers. SDK-free so the UI and tests don't depend on the bindings.
+ */
+enum class WithdrawOnchainSpeed(val label: String, val detail: String) {
+    SLOW("Economy", "Cheapest. May take hours to confirm."),
+    MEDIUM("Standard", "Balanced fee and confirmation time."),
+    FAST("Priority", "Highest fee. Confirms soonest.")
+}
+
+/**
+ * What a withdrawal would cost and deliver, quoted before anything is signed.
+ *
+ * [spendSats] is the whole spendable balance and [feeSats] comes out of it -
+ * the SDK's FeePolicy.FEES_INCLUDED - so [netSats] is what actually lands at
+ * the destination. Quoting this way is the only honest way to drain: with
+ * fees added on top, a send of the full balance can never succeed.
+ */
+data class WithdrawOnchainQuote(
+    val address: String,
+    val spendSats: Long,
+    val feeSats: Long,
+    val speed: WithdrawOnchainSpeed
+) {
+    val netSats: Long get() = (spendSats - feeSats).coerceAtLeast(0L)
+
+    /**
+     * True when the fee would consume everything. Spark's dust and fee floors
+     * mean a small balance can be genuinely unspendable on-chain - better to
+     * say so than to broadcast something that delivers zero.
+     */
+    val isUneconomical: Boolean get() = netSats <= 0L
+
+    /**
+     * Share of the balance eaten by fees, for the warning copy. Draining a
+     * small balance can cost a large percentage, and that should be visible
+     * before confirming rather than discovered after.
+     */
+    val feePercent: Double
+        get() = if (spendSats <= 0L) 0.0 else (feeSats.toDouble() / spendSats.toDouble()) * 100.0
+}
+
+/**
+ * Funds a withdrawal cannot move, reported after the fact.
+ *
+ * "Recover everything" is not literally achievable: tokens below the
+ * provider's conversion floor cannot be converted at any price, and Spark
+ * leaves worth less than their own exit cost are not worth spending. Naming
+ * the remainder is more useful than implying it does not exist.
+ */
+data class WithdrawOnchainRemainder(
+    /** Ticker to human-readable amount left behind, e.g. "USDB" to "0.34". */
+    val strandedTokens: Map<String, String> = emptyMap(),
+    val strandedSats: Long = 0L
+) {
+    val isEmpty: Boolean get() = strandedTokens.isEmpty() && strandedSats == 0L
+}
+
+/**
+ * Pull a bare address out of whatever a wallet's QR actually encodes.
+ *
+ * Bitcoin QRs are usually BIP-21 URIs - "bitcoin:bc1q...?amount=0.01&label=x"
+ * - and the SDK's parser wants the address alone. The amount is deliberately
+ * discarded: this screen always sends the whole balance, so honoring a
+ * requested amount would contradict what the button says.
+ */
+fun normalizeBitcoinAddress(raw: String): String {
+    var value = raw.trim()
+    // Anchored and case-insensitive: some wallets emit "BITCOIN:", and an
+    // address containing "bitcoin:" mid-string must not be mangled.
+    if (value.length >= 8 && value.substring(0, 8).equals("bitcoin:", ignoreCase = true)) {
+        value = value.substring(8)
+    }
+    val query = value.indexOf('?')
+    if (query >= 0) value = value.substring(0, query)
+    return value.trim()
+}

app/src/main/kotlin/com/wisp/app/ui/screen/WalletScreen.kt
@@ -91,6 +91,7 @@ import androidx.compose.material.icons.filled.Receipt
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.SwapHoriz
+import androidx.compose.material.icons.filled.ArrowOutward
 import androidx.compose.material.icons.filled.CloudUpload
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.AlertDialog
@@ -126,6 +127,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import com.wisp.app.ui.component.WithdrawOnchainSheet
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -218,6 +220,21 @@ fun WalletScreen(
 ) {
     val walletState by viewModel.walletState.collectAsState()
     val currentPage by viewModel.currentPage.collectAsState()
+    var showWithdrawOnchain by remember { mutableStateOf(false) }
+
+    if (showWithdrawOnchain) {
+        WithdrawOnchainSheet(
+            onQuote = { address, speed -> viewModel.sparkRepo.prepareWithdrawOnchain(address, speed) },
+            onConfirm = { quote ->
+                val result = viewModel.sparkRepo.executeWithdrawOnchain(quote)
+                // Reflect the emptied balance and the new row without waiting
+                // for the next poll — the user just moved everything.
+                if (result.isSuccess) viewModel.refreshState()
+                result
+            },
+            onDismiss = { showWithdrawOnchain = false }
+        )
+    }
 
     // Always refresh wallet state when this screen appears
     LaunchedEffect(Unit) {
@@ -613,6 +630,12 @@ fun WalletScreen(
                         },
                         onExportConnectionString = { viewModel.showNwcExport() },
                         onDeleteWallet = { viewModel.navigateTo(WalletPage.DeleteWalletConfirm) },
+                        // Spark only: passing null hides the row, so an NWC
+                        // wallet can't reach a button with no on-chain send
+                        // command behind it.
+                        onWithdrawOnchain = if (viewModel.walletMode.value == WalletMode.SPARK) {
+                            { showWithdrawOnchain = true }
+                        } else null,
                         relayBackupStatuses = viewModel.relayBackupStatuses.collectAsState().value,
                         relayBackupCheckLoading = viewModel.relayBackupCheckLoading.collectAsState().value,
                         deleteBackupStatus = viewModel.deleteBackupStatus.collectAsState().value,
@@ -4796,6 +4819,8 @@ private fun WalletSettingsContent(
     onBackupToRelay: () -> Unit = {},
     onExportConnectionString: () -> Unit = {},
     onDeleteWallet: () -> Unit,
+    /** Spark only — NWC has no on-chain send command. */
+    onWithdrawOnchain: (() -> Unit)? = null,
     relayBackupStatuses: List<RelayBackupInfo> = emptyList(),
     relayBackupCheckLoading: Boolean = false,
     deleteBackupStatus: DeleteBackupStatus = DeleteBackupStatus.Idle,
@@ -5159,6 +5184,44 @@ private fun WalletSettingsContent(
                         style = MaterialTheme.typography.bodyLarge,
                         color = Color(0xFFFF3B30)
                     )
+                }
+            }
+            if (onWithdrawOnchain != null) {
+                Spacer(Modifier.height(8.dp))
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable(onClick = onWithdrawOnchain),
+                    colors = CardDefaults.cardColors(
+                        containerColor = MaterialTheme.colorScheme.surfaceVariant.copy(alpha = 0.5f)
+                    )
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp, vertical = 14.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            Icons.Default.ArrowOutward,
+                            contentDescription = null,
+                            tint = Color(0xFFFF3B30),
+                            modifier = Modifier.size(22.dp)
+                        )
+                        Spacer(Modifier.width(12.dp))
+                        Column {
+                            Text(
+                                "Withdraw on-chain",
+                                style = MaterialTheme.typography.bodyLarge,
+                                color = Color(0xFFFF3B30)
+                            )
+                            Text(
+                                "Send everything to a Bitcoin address",
+                                style = MaterialTheme.typography.bodySmall,
+                                color = MaterialTheme.colorScheme.onSurfaceVariant
+                            )
+                        }
+                    }
                 }
             }
             Spacer(Modifier.height(8.dp))

app/src/test/kotlin/com/wisp/app/repo/WithdrawOnchainTest.kt
@@ -1,0 +1,148 @@
+package com.wisp.app.repo
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+/**
+ * The arithmetic behind the Withdraw on-chain confirmation screen, and the
+ * BIP-21 address normalization. Pure, and worth pinning: these numbers are
+ * what a user reads before irreversibly emptying their wallet.
+ *
+ * Ported from wisp-ios#452.
+ */
+class WithdrawOnchainTest {
+
+    private fun quote(
+        spend: Long,
+        fee: Long,
+        speed: WithdrawOnchainSpeed = WithdrawOnchainSpeed.MEDIUM
+    ) = WithdrawOnchainQuote("bc1qexample", spend, fee, speed)
+
+    // ---- Quote arithmetic ----
+
+    /** The fee comes OUT of the balance (FEES_INCLUDED). */
+    @Test
+    fun `net is balance minus fee`() {
+        assertEquals(97_500L, quote(100_000, 2_500).netSats)
+    }
+
+    /** Never advertise a negative amount. */
+    @Test
+    fun `net clamps at zero`() {
+        assertEquals(0L, quote(1_000, 5_000).netSats)
+    }
+
+    @Test
+    fun `a fee that eats everything is uneconomical`() {
+        assertTrue(quote(1_000, 5_000).isUneconomical)
+        assertTrue(quote(1_000, 1_000).isUneconomical)
+    }
+
+    @Test
+    fun `an ordinary drain is economical`() {
+        assertFalse(quote(100_000, 2_500).isUneconomical)
+    }
+
+    @Test
+    fun `fee percent is share of balance`() {
+        assertEquals(10.0, quote(100_000, 10_000).feePercent, 0.001)
+        assertEquals(25.0, quote(20_000, 5_000).feePercent, 0.001)
+    }
+
+    /** No division by zero on an empty wallet. */
+    @Test
+    fun `fee percent on an empty balance is zero`() {
+        assertEquals(0.0, quote(0, 500).feePercent, 0.001)
+    }
+
+    /**
+     * Equality gates execution: executeWithdrawOnchain refuses unless the held
+     * quote matches the one the user confirmed, so a drifted screen can't send
+     * different terms than were signed off.
+     */
+    @Test
+    fun `quotes differing in any field are not equal`() {
+        val base = quote(100_000, 2_500)
+        assertTrue(base != quote(100_001, 2_500))
+        assertTrue(base != quote(100_000, 2_600))
+        assertTrue(base != quote(100_000, 2_500, WithdrawOnchainSpeed.FAST))
+        assertTrue(base != WithdrawOnchainQuote("bc1qother", 100_000, 2_500, WithdrawOnchainSpeed.MEDIUM))
+        assertEquals(base, quote(100_000, 2_500))
+    }
+
+    @Test
+    fun `every speed is labelled and explained`() {
+        WithdrawOnchainSpeed.entries.forEach {
+            assertTrue(it.label.isNotEmpty())
+            assertTrue(it.detail.isNotEmpty())
+        }
+    }
+
+    // ---- Remainder ----
+
+    @Test
+    fun `nothing stranded is empty`() {
+        assertTrue(WithdrawOnchainRemainder().isEmpty)
+    }
+
+    /** Tokens under the conversion floor can't move at any price. */
+    @Test
+    fun `stranded tokens are not empty`() {
+        assertFalse(WithdrawOnchainRemainder(strandedTokens = mapOf("USDB" to "0.34")).isEmpty)
+    }
+
+    @Test
+    fun `stranded sats are not empty`() {
+        assertFalse(WithdrawOnchainRemainder(strandedSats = 120).isEmpty)
+    }
+
+    // ---- BIP-21 normalization ----
+
+    @Test
+    fun `a bare address passes through`() {
+        assertEquals("bc1qexample", normalizeBitcoinAddress("bc1qexample"))
+    }
+
+    @Test
+    fun `strips the bip21 scheme`() {
+        assertEquals("bc1qexample", normalizeBitcoinAddress("bitcoin:bc1qexample"))
+    }
+
+    /** Some wallets emit an uppercase scheme. */
+    @Test
+    fun `scheme match is case insensitive`() {
+        assertEquals("bc1qexample", normalizeBitcoinAddress("BITCOIN:bc1qexample"))
+    }
+
+    /**
+     * The amount is discarded on purpose: this screen always sends the whole
+     * balance, so honoring a requested amount would contradict the button.
+     */
+    @Test
+    fun `drops query parameters`() {
+        assertEquals(
+            "bc1qexample",
+            normalizeBitcoinAddress("bitcoin:bc1qexample?amount=0.01&label=x")
+        )
+    }
+
+    @Test
+    fun `trims whitespace and newlines`() {
+        assertEquals("bc1qexample", normalizeBitcoinAddress("  bc1qexample\n"))
+    }
+
+    /** "bitcoin" mid-string is not a scheme - an address must not be mangled. */
+    @Test
+    fun `only an anchored scheme is stripped`() {
+        assertEquals("bc1qbitcoin:x", normalizeBitcoinAddress("bc1qbitcoin:x"))
+    }
+
+    /** Shorter than the scheme itself must not throw. */
+    @Test
+    fun `short input is safe`() {
+        assertEquals("bc1", normalizeBitcoinAddress("bc1"))
+        assertEquals("", normalizeBitcoinAddress(""))
+    }
+}